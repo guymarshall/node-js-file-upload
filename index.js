480721cf09f58f032d87eaf3f8e8893eae7b7e57
Guy Marshall <guymarshall9@gmail.com>
2022-08-21 18:44:34 +0100

    Add semicolons to ends of lines

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const express = require('express')
-const app = express()
-const port = 8080
+const express = require('express');
+const app = express();
+const port = 8080;
 
 const currentDate = new Date().toLocaleDateString();
 
 app.get('/', (req, res) => {
-  res.send(`Hello World! Current date: ${currentDate}`)
-})
+  res.send(`Hello World! Current date: ${currentDate}`);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
