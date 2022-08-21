29a2d6bb68e97a487bf59e1f33c4b087300360a9
Guy Marshall <guymarshall9@gmail.com>
2022-08-21 18:41:09 +0100

    Add currentDate to output

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express')
 const app = express()
 const port = 8080
 
+const currentDate = new Date().toLocaleDateString();
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send(`Hello World! Current date: ${currentDate}`)
 })
 
 app.listen(port, () => {
